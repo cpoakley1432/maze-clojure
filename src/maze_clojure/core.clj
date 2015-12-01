c09aac68e9405a6eae5fedecd53c4d6d52cc5481
cpoakley1432 <cpoakley1432@gmail.com>
2015-12-01 11:16:53 -0500

    create maze

--- src/maze_clojure/core.clj
+++ src/maze_clojure/core.clj
@@ -15,8 +15,46 @@
    (get-in rooms [row (- col 1)])
    (get-in rooms [row (+ col 1)])])
 
+(defn random-neighbor [rooms row col]
+  (let [neighbors (possible-neighbors rooms row col)
+        neighbors (filter (fn [room]
+                            (and room
+                                 (not (:visited? room))))
+                          neighbors)]
+    (if (pos? (count neighbors))
+      (rand-nth neighbors)
+      nil)))
+
+(defn tear-down-wall [rooms old-row old-col new-row new-col]
+  (cond
+    ; going up
+    (< new-row old-row)
+    (assoc-in rooms [new-row new-col :bottom?] false)
+    ; going down
+    (> new-row old-row)
+    (assoc-in rooms [old-row old-col :bottom?] false)
+    ; going left
+    (< new-col old-col)
+    (assoc-in rooms [new-row new-col :right?] false)
+    ; going right
+    (> new-col old-col)
+    (assoc-in rooms [old-row old-col :right?] false)))
+
+(defn create-maze [rooms row col]
+  (let [rooms (assoc-in rooms [row col :visited?] true)
+        next-room (random-neighbor rooms row col)]
+    (if next-room
+      (let [rooms (tear-down-wall rooms row col (:row next-room) (:col next-room))]
+        (loop [old-rooms rooms]
+          (let [new-rooms (create-maze old-rooms (:row next-room) (:col next-room))]
+            (if (= old-rooms new-rooms)
+              old-rooms
+              (recur new-rooms)))))
+      rooms)))
+
 (defn -main [& args]
-  (let [rooms (create-rooms)]
+  (let [rooms (create-rooms)
+        rooms (create-maze rooms 0 0)]
     ; print top walls
     (doseq [row rooms]
       (print " _"))
